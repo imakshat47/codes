1918c639aa70a3050f4bccdee8e1929667375bf5
unknown <imakshat47@gmail.com>
2025-03-15 15:58:03 +0530

    Update file: ADS/dijkstras.cpp

--- ADS/dijkstras.cpp
+++ ADS/dijkstras.cpp
@@ -1,77 +1,72 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
 
 using namespace std;
 
-class Graph{
-	int V;
-	int t = 0;
-	vector<int> path;
-	vector< vector<int> > adj;
-	
-	void DFS_util(int s, vector<bool>& visited){			
-		visited[s]=true;		
-		//t++;
-		path.push_back(s);
-		for(int i: adj[s]){			
-			++t;
-			if(!visited[i])
-				DFS_util(i,visited);
-		}
-	}
-	
-	public:
-	Graph(int v){
-		V=v;
-		adj.resize(v);
-		//path.resize(v);
-	}
-	
-	void addEdge(int v, int u){
-		adj[v].push_back(u);
-		adj[u].push_back(v);
-	}
-	
-	void DFS(int s){
-		vector <bool> visited(V,false);
-		DFS_util(s,visited);
-	}
-	
-	void printPath(){
-		for(int i:path){			
-			cout<<i<<" ";
-		}
-	}
-	
-	int T(){return t;}
-	
+class Graph {
+    int V;
+    vector<vector<pair<int, int>>> adj; // Adjacency list (node, weight)
+
+public:
+    Graph(int v) {
+        V = v;
+        adj.resize(v);
+    }
+
+    void addEdge(int u, int v, int weight) {
+        adj[u].push_back({v, weight});
+        adj[v].push_back({u, weight}); // Remove this for a directed graph
+    }
+
+    void dijkstra(int src) {
+        vector<int> dist(V, INT_MAX);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        dist[src] = 0;
+        pq.push({0, src});
+
+        while (!pq.empty()) {
+            int u = pq.top().second;
+            pq.pop();
+
+            for (auto &[v, weight] : adj[u]) {
+                if (dist[u] + weight < dist[v]) {
+                    dist[v] = dist[u] + weight;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+
+        cout << "Shortest distances from source " << src << ":\n";
+        for (int i = 0; i < V; i++) {
+            cout << "Node " << i << " -> Distance: " << dist[i] << endl;
+        }
+    }
 };
 
-int main(){
-	cout<<"No of Vertices: ";
-	int v;cin>>v;
+int main() {
+    cout << "Enter number of vertices: ";
+    int V, E;
+    cin >> V;
+    Graph g(V);
 
-	Graph g(v);
-	cout<<"No of Edges: ";
-	
-	int e;cin>>e;
-	cout<<"Ënter "<<e<<" edges: "<<endl;
-	for(int i=0;i<e;++i){
-		int v,u;cin>>v>>u;
+    cout << "Enter number of edges: ";
+    cin >> E;
 
-		g.addEdge(v,u);
-	}	
-	
-	cout<<"Enter source vertex: ";
-	int s;cin>>s;
-	g.DFS(s);
-	cout<<"Path: ";
-	g.printPath();
-	cout<<endl;
-	cout<<endl<<"Total time complexity O(V+E)= "<<g.T();
-	cout<<endl;
-	cout<<endl<<"Auxiliary space complexity O(V+E)= "<<v+e;
-	cout<<endl;
-	
-	return 0;
+    cout << "Enter " << E << " edges (u v weight):\n";
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g.addEdge(u, v, w);
+    }
+
+    cout << "Enter source vertex: ";
+    int src;
+    cin >> src;
+    
+    g.dijkstra(src);
+
+    return 0;
 }
